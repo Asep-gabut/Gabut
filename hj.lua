0d8e99e283de2fbfa5a92ff06665f47f4c599d2c
Asep <linddermawan@gmail.com>
2026-09-23 10:47:03 +0800

    Update hj.lua

--- hj.lua
+++ hj.lua
@@ -6,11 +6,11 @@
 
 local CONFIG = {
     KeepDistance = 45,
-    KiteMinDistance = 60,
-    KiteMaxDistance = 150,
+    KiteMinDistance = 20,
+    KiteMaxDistance = 60,
     KiteDistanceStep = 3,
     KiteAngleCount = 30,
-    KiteMaxChecks = 900,         -- batas ComputeAsync biar ga lag
+    KiteMaxChecks = 399,         -- batas ComputeAsync biar ga lag
     AttackCooldown = 0.1,
     LoopDelay = 0.03,
     
